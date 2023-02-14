1e7111995939c333d8e9d6b230ffcc131f3b8489
Anton Fedoruk <antobn.fedoruk@gmail.com>
2023-02-14 16:51:10 +0200

    BGGSTGB-0: Create skeleton BGGSTGB Spring Boot project.

---
+++ src/test/java/com/github/antonfedoruk/boardgamesgooglesheettgbot/BoardGamesGoogleSheetTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonfedoruk.boardgamesgooglesheettgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGamesGoogleSheetTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
